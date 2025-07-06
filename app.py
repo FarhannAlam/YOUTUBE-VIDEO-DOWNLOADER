c4f4bdb5809f16f519583ccf97fae7519451f260
FarhannAlam <farhanalam5192@gmail.com>
2025-07-07 01:14:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,3 +37,8 @@ def download():
 if __name__ == '__main__':
     os.makedirs(DOWNLOAD_DIR, exist_ok=True)
     app.run(debug=True)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
